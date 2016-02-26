b09fed3cbc36c3db53a2ee2107cb2c268d52196e
Malkie Golub <Malkie Golub>
2016-02-25 21:38:09 -0500

    initial commit

---
+++ src/main/java/golub/flowFree_Golub_Yarmark/App.java
@@ -0,0 +1,13 @@
+package golub.flowFree_Golub_Yarmark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
